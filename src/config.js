b8502c672633954b3f5b06aa4bfe53f3739cc4b6
Ivan L. Alvarez <ivanlalvarez.22@gmail.com>
2024-10-01 00:11:14 -0300

    config.js updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://gd53db65-5000.brs.devtunnels.ms/api/v1"; //"http://localhost:5000/api/v1";
+export const BASE_URL = "https://dr-turno-backend.vercel.app/api/v1"; //"http://localhost:5000/api/v1";
 export const token = localStorage.getItem("token");
